9b11a92b382cf87c471880727cba4e0f9aebcb80
isaiahmathieu <isaiah.mathieu@gmail.com>
2014-05-18 15:13:28 -0700

    committing before changing after save to before save for Event

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var serverHostname = 'klement.cs.washington.edu';
+//var serverHostname = 'klement.cs.washington.edu';
+var serverHostname = '128.208.7.228'; 
 var serverPort = 9876;
 var defaultChannel = 'default';
+var loudUnknownChannel = 'loudUnknown';
 
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
@@ -25,6 +27,28 @@ Parse.Cloud.afterSave("Sound", function(request) {
 Parse.Cloud.afterDelete("Sound", function(request) {
 	var filename = request.object.id;
 	var urlAndPath = serverHostname + ':' + serverPort + '/deleteTarget?filename=';
+	urlAndPath += filename;
+	// right now the file name does not include the .wav extension, so add it
+	// here
+	urlAndPath += '.wav';
+	Parse.Cloud.httpRequest({
+		method: 'GET',
+		url: urlAndPath,
+		success: function(httpResponse) {
+			console.log('success back from the nodejs server');	
+			console.log(httpResponse);
+		},
+		error: function(httpResponse) {
+			console.log('error response from nodejs server');	
+			console.log(httpResponse);
+		}		 
+	});	
+});
+
+
+Parse.Cloud.afterDelete("Event", function(request) {
+	var filename = request.object.get('eventFilename');
+	var urlAndPath = serverHostname + ':' + serverPort + '/deleteEventRecording?filename=';
 	urlAndPath += filename;
 	Parse.Cloud.httpRequest({
 		method: 'GET',
@@ -58,6 +82,18 @@ function setNextAsTarget(request) {
 		}
 	});
 }
+/*
+Parse.Cloud.beforeSave("Event", function(request, response) {
+	var matched = request.object.get("match");
+	console.log('before saving, found a match: ' + matched);	
+	if (matched) {
+		// get the filename of the match
+		var query = new Parse
+		request.object.set("matchSoundName", 	
+	}
+	response.success();
+});
+*/
 
 Parse.Cloud.afterSave("Event", function(request) {
 	console.log("Event occurred");
@@ -70,17 +106,18 @@ Parse.Cloud.afterSave("Event", function(request) {
 	var matchFound = request.object.get("match");
 	var eventFilename = request.object.get("eventFilename");
 	if (matchFound) {	
+		// matchFilename is actually a misnomer because it is not the whole
+		// name of the file that the event sound was matched too, its the 
+		// filename with the .wav extension removed. Thus it is just the objectId
+		// of the Sound object
 		var matchFilename = request.object.get("matchFilename");
 		var query = new Parse.Query("Sound");
 		console.log('looking for Sound object with id: ' + matchFilename);
 		query.get(matchFilename, {
 			success: function(result) {
+				var matchSoundName = result.get("name");
 				if (result.get("enabled")) {
-					sendPushForMatch(result.get("name"), eventFilename);
-					// now add the name of the match sound to the Event object so it
-					// can be displayed in the app without having to do an extra query
-					request.object.set('matchSoundName', result.get('name'));
-					request.object.save();
+					sendPushForMatch(matchSoundName, eventFilename);
 				}
 			}, 
 			error: function() {
@@ -88,18 +125,38 @@ Parse.Cloud.afterSave("Event", function(request) {
 			}
 		});
 	} else {
-		sendPushForNoMatch(eventFilename);
+		// first check to see if the user has notifications for loud unidentified
+		// noises turned on
+			sendPushForNoMatch(eventFilename);
+	/*	
+		if (false) {
+			sendPushForNoMatch(eventFilename);
+		} else {
+			// delete this event from Parse 
+			request.object.destroy({
+				success : function() {
+					console.log('successfully deleted event object');	
+				},
+				error : function(object, error) {
+					console.log('failed to delete object: ' + object);
+					console.log('error: ' + error);
+				}
+
+			});		
+			// delete the recording from the nodejs server
+		}
+	*/
 	}
 });
 
 
 function sendPushForNoMatch(filename) {
 	Parse.Push.send({
-		channels: [defaultChannel],
+		channels: [loudUnknownChannel],
 		data: {
 			alert: "Loud unidentified noise heard",
 			filename : filename,
-			action: "org.cs.washington.cse477.SOUND_DATA_GETTER",
+			//action: "org.cs.washington.cse477.SOUND_DATA_GETTER",
 			match: false
 		}
 	}, {
@@ -121,7 +178,7 @@ function sendPushForMatch(soundName, eventRecordingFilename) {
 		data: {
 			alert: "noise detected: " + soundName,
 			filename : eventRecordingFilename,
-			action: "org.cs.washington.cse477.SOUND_DATA_GETTER",
+			//action: "org.cs.washington.cse477.SOUND_DATA_GETTER",
 			match: true
 		}
 	}, {
