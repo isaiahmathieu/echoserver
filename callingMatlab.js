0fe3c61e17980c4f7c76ac29e5aeff4475e33bd5
isaiahmathieu <isaiah.mathieu@gmail.com>
2014-05-17 18:40:16 -0700

    works. had to escape the quotes

--- callingMatlab.js
+++ callingMatlab.js
@@ -5,32 +5,33 @@ var fs = require('fs');
 
 var resultsFilename = 'results.txt';
 
-var filename = 'sup.txt';
+var filename = 'dedede.wav';
 
-const matchAgainst = './matchAgainst/';
-const eventRecordings = './eventRecordings/';
+const matchAgainst = 'matchAgainst/';
+const eventRecordings = 'eventRecordings/';
 
 
 var matlabPath = process.env.MATLAB_BINARY;
 
 // build up command starting with the path to the matlab binary
-var cmd = matlabPath + 'matlab -nojvm -nodisplay -nosplash -r "matchSounds ';
+var cmd = matlabPath + 'matlab -nojvm -nodisplay -nosplash -r \"matchSounds ';
 // now add arguments. first the path to the event recordings folder 
-cmd += '"' + __dirname + '/' + eventRecordings + filename + '" ';
+cmd += '\"' +  __dirname + '/' + eventRecordings + filename + '\" ';
 // add the second argument which is the folder with the sounds to match against
-cmd += '"' + __dirname + '/' + matchAgainst + '" ';
+cmd += '\"' + __dirname + '/' + matchAgainst + '\" ';
 // add the last argument which is the path+name of the file to write the results to
-cmd += '"' + __dirname + '/' + resultsFilename + '"';
+cmd += '\"' + __dirname + '/' + resultsFilename;
 // add the exit command to exit matlab
-cmd += ';exit"';
+cmd += '\";exit;\"';
 console.log(cmd);
 
 //cmd += '\"homeSounds/dedede.wav\" \"homeSounds\/\" \"' + resultsFilename + '\";exit\"';
-/*
+
 worker.exec(cmd, function(err, out, stderr) {
+	//console.log(out);
 	checkResults();
 });
-*/
+
 function checkResults() {
 	var result = fs.readFileSync(resultsFilename);
 	if (result === 'NO_MATCH') {
